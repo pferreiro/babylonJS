25f6422623e880ba49a626359dd507fbd7ee6058
pferreiro <pedroferreiro95@gmail.com>
2023-09-25 17:24:59 +0100

    buttons

--- js/main.js
+++ js/main.js
@@ -11,33 +11,58 @@ const createScene = async function () {
   camera.attachControl(canvas, true);
   const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
   light.intensity = 0.7;
+  // var assetsManager = new BABYLON.AssetsManager(scene);
+  scene.createDefaultCameraOrLight(true, true, true);
+  scene.createDefaultEnvironment();
+  var objects = ['19 Heineken Chapeu.glb', '20 21 Heineken Esplanada.glb', '23 Heineken Bidon.glb', '25 Heineken Coluna.glb']
 
-  var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
-  groundMaterial.specularColor = BABYLON.Color3.Black();
-  var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 5, height: 5 }, scene, false);
-  ground.material = groundMaterial;
+  // GUI
+  var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+  var panel = new BABYLON.GUI.StackPanel();
+  advancedTexture.addControl(panel);
 
-  BABYLON.SceneLoader.ImportMesh(
-    "",
-    "https://firebasestorage.googleapis.com/v0/b/yes4eutests.appspot.com/o/centralcervejas%2Fars%2",
-    "F1693402803018_Sagres%20Esplanada%20Bege%20Mesa%20Alta.glb?alt=media&token=e6ea8e76-8163-4f5f-87f1-84d794677f95",
-    scene,
-    function (meshes) {
-      const model = meshes[0]
-      // console.log(model)
-      scene.createDefaultCameraOrLight(true, true, true);
-      scene.createDefaultEnvironment();
+  var createButtons = function (object) {
+    var button = BABYLON.GUI.Button.CreateSimpleButton(object, object);
+    button.width = "150px"
+    button.height = "150px";
+    button.color = "white";
+    button.cornerRadius = 5;
+    button.background = "green";
+    button.top = "15px"
+    button.left = "15px"
+    button.horizontalAlignment = 0
+    button.verticalAlignment = 0
+    button.onPointerUpObservable.add(function () {
+      BABYLON.SceneLoader.ImportMesh("", "assets/scenes/", object, scene, function (meshes) {
+        var pointerDragBehavior = new BABYLON.PointerDragBehavior({ dragPlaneNormal: new BABYLON.Vector3(0, 0.1, 0) });
+        pointerDragBehavior.useObjectOrientationForDragging = false;
+        const model = meshes[0]
 
-      model.position = new BABYLON.Vector3(0, 0, 1)
-
-      // var sixDofDragBehavior = new BABYLON.SixDofDragBehavior()
-      // model.addBehavior(sixDofDragBehavior)
-
-
-      var pointerDragBehavior = new BABYLON.PointerDragBehavior({ dragPlaneNormal: new BABYLON.Vector3(0, 0.1, 0) });
-      pointerDragBehavior.useObjectOrientationForDragging = false;
-      model.addBehavior(pointerDragBehavior);
+        model.position = new BABYLON.Vector3(0, 0, 1)
+        model.rotation = BABYLON.Vector3.Zero()
+        model.addBehavior(pointerDragBehavior);
+        // console.log(model)
+      });
     });
+    panel.addControl(button);
+  }
+
+  for (let i = 0; i < objects.length; i++) {
+    createButtons(objects[i])
+  }
+
+  var buttonCamera = BABYLON.GUI.Button.CreateSimpleButton('camera', '');
+  buttonCamera.width = "50px"
+  buttonCamera.height = "50px";
+  buttonCamera.color = "white";
+  buttonCamera.cornerRadius = 50;
+  buttonCamera.bottom = "50px"
+  buttonCamera.horizontalAlignment = 2
+  buttonCamera.verticalAlignment = 1
+  buttonCamera.onPointerUpObservable.add(function () {
+    BABYLON.Tools.CreateScreenshotUsingRenderTarget(engine, camera, 400);
+  });
+  advancedTexture.addControl(buttonCamera);
 
   var xr = await scene.createDefaultXRExperienceAsync({
     uiOptions: {
@@ -48,6 +73,7 @@ const createScene = async function () {
 
   });
 
+  // assetsManager.load();
   const fm = xr.baseExperience.featuresManager
   const xrBackgroundRemover = fm.enableFeature(BABYLON.WebXRBackgroundRemover.Name);
   return scene;
